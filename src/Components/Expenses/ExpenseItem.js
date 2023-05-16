1244c8f327027554e01f58b0a8cf6272a5f999bc
Manika <manika.aditya@gmail.com>
2023-05-16 12:53:34 +0530

    updated dynamic lists of data

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,19 +5,12 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-// function clickHander()
-const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () =>{
-    setTitle('updated');
-  };
   return (
     <Card className="expense-item">
     <ExpenseDate date = {props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
